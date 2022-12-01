3b081faa0e59b4c047e9d653bb997c53a736ed0a
mmatczak <marek.matczak79@gmail.com>
2022-12-01 11:21:37 +0100

    master details

--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,4 +1,7 @@
 import {Book} from '../model';
+import {map, Observable, Subscription} from 'rxjs';
+
+// type FindByIdCallbackFn = (book: Book) => void;
 
 export class BookService {
   private readonly books = [
@@ -31,4 +34,96 @@ export class BookService {
   findAll(): Book[] {
     return this.books;
   }
+
+  findById(bookId: number): Observable<Book> {
+    return new Observable<Book>(subscriber => {
+      setTimeout(() => {
+        const book = this.books.find(book => book.id === bookId);
+        if (book) {
+          subscriber.next(book);
+          subscriber.complete();
+        } else {
+          subscriber.error(new Error(`Book with ID ${bookId} could not be found!`));
+        }
+      }, 1000);
+    })
+
+
+    // return fetch(`books/${bookId}`)
+    //   .then(response => response.json());
+    // return new Promise<Book>((resolve, reject) => {
+    //   // setTimeout(() => {
+    //     const book = this.books.find(book => book.id === bookId);
+    //     if (!book) {
+    //       reject(new Error(`Book with ID ${bookId} could not be found!`))
+    //     } else {
+    //       resolve(book);
+    //     }
+    //   // });
+    // });
+  }
 }
+
+const service = new BookService();
+// 1. sync
+// try {
+//   const book = service.findById(123);
+//   console.log(book);
+//   return book;
+// } catch (e) {
+//   console.error(e);
+//   throw e;
+// }
+// return undefined;
+
+// 2. async (callbacks)
+// service.findById(123,
+//   function (book) {
+//     console.log(book);
+//     service.findById(646, book2 => {
+//
+//     })
+//   });
+console.log('End');
+
+// 3. async (Promises)
+// service.findById(123)
+//   .then(
+//     function (book) {
+//       console.log(book);
+//     },
+//     error => {
+//       console.error(error);
+//       return Promise.reject();
+//     }
+//   )
+// console.log("End");
+
+// async function f() {
+//   try {
+//     const book = await service.findById(123);
+//     console.log(book);
+//   } catch (e) {
+//     console.error(e);
+//     throw e;
+//   }
+// }
+
+// 4. async (Observables RxJS)
+const book$ = service.findById(113)
+  .pipe(map(book => book.id));
+const subscription: Subscription = book$
+  .subscribe({
+    next(book) {
+      console.log(book);
+    },
+    error(error) {
+      console.error(error);
+    },
+    complete() {
+      console.error('Done..');
+    }
+  })
+
+subscription.unsubscribe();
+
